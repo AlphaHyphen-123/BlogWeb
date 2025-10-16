2fc77c873ea82b8092d76d513a53791095104460
Shivam Sen <shivamsen9644@gmail.com>
2025-10-16 12:16:18 +0530

    frontend main change

--- frontend/src/component/Login.jsx
+++ frontend/src/component/Login.jsx
@@ -15,7 +15,7 @@ function Login() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const res = await fetch('/api/login', {
+      const res = await fetch(`${import.meta.env.VITE_API_URL}/api/signup`, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(formData)
